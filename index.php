306b2b7b84742b2bd35c83e9cffbdf61f72cbe3e
matheus <mfscontato2196@gmail.com>
2021-10-24 13:16:55 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!--Bootstrap-->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <!--CSS-->
+    <link rel="stylesheet" href="css/style.css">
+
+    <title>Create</title>
+</head>
+<body>
+    <div class="container">
+        <form>
+            <div class="form-group">
+            <label for="name">Name</label>
+            <input type="email" class="form-control" id="name"  placeholder="Your name">
+            </div>
+
+            <div class="form-group">
+            <label for="email">Email</label>
+            <input type="email" class="form-control" id="email"  placeholder="example@ex.com">
+            </div>
+        
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+    </div>
+    
+</body>
+</html>
